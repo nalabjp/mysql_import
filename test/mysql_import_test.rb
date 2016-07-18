267e7132695196b63146e7c0c9072c540bbe8d80
nalabjp <nalabjp@gmail.com>
2016-07-18 23:45:02 +0900

    :green_heart: Simple test of `MysqlImport#import`

--- test/mysql_import_test.rb
+++ test/mysql_import_test.rb
@@ -1,11 +1,19 @@
 require 'test_helper'
 
-class MysqlImportTest < Minitest::Test
-  def test_that_it_has_a_version_number
+class MysqlImportTest < Test::Unit::TestCase
+  test 'It has a version number' do
     refute_nil ::MysqlImport::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  sub_test_case '#import' do
+    teardown do
+      DbHelper.truncate('users')
+    end
+
+    test 'success' do
+      client = MysqlImport.new(DbConfig.to_hash)
+      client.add(File.expand_path('../csv/users_valid.csv', __FILE__), table: 'users')
+      client.import
+    end
   end
 end
